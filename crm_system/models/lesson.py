533701c8c6423a6924455eed3ac215f2bd255b66
Ivan <lapchenko.vanya@gmail.com>
2023-01-20 13:24:49 +0200

    Add table lesson

---
+++ crm_system/models/lesson.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
+
+from crm_system.models.database import Base
+
+association_table = Table('association', Base.metadata,
+                          Column('lesson_id', Integer, ForeignKey('lessons.id')),
+                          Column('group_id', Integer, ForeignKey('groups.id'))
+                          )
+
+
+class Lesson(Base):
+    __tablename__ = 'lessons'
+
+    id = Column(Integer, primary_key=True)
+    lesson_title = Column(String)
+    groups = relationship('Group', secondary=association_table, backref='group_lesson')
+
+    def __repr__(self):
+        return f'Lesson [ID: {self.id}, Name: {self.lesson_title}]'
