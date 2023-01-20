a1abb3633f0aa1d7c60ae79b8e320972ff12fa69
Ivan <lapchenko.vanya@gmail.com>
2023-01-20 15:50:27 +0200

    Edit routes

--- crm_system/models/database.py
+++ crm_system/models/database.py
@@ -4,11 +4,12 @@ from sqlalchemy.orm import sessionmaker
 
 db_name = 'app.db'
 
-engine = create_engine(f'sqlite:///{db_name}')
-Session = sessionmaker(bind=engine)
+database = create_engine(f'sqlite:///{db_name}')
+Session = sessionmaker(bind=database)
+session = Session()
 
 Base = declarative_base()
 
 
 def create_db():
-    Base.metadata.create_all(engine)
+    Base.metadata.create_all(database)
--- crm_system/routes.py
+++ crm_system/routes.py
@@ -1,7 +1,22 @@
-from flask import render_template
+from flask import render_template, request
 from crm_system import app
+from crm_system.models.database import session, database
+from crm_system.models.group import Group
+
+
+
 
 
 @app.route("/")
 def index():
     return render_template("main.html")
+
+
+@app.route("/group_management", methods=["GET", "POST"])
+def group_management():
+    if request.method == "POST":
+        gr_name = request.form["gr_name"]
+        group = Group(group_name=gr_name)
+        database.session.add(group)
+        database.session.commit()
+    return render_template("group_management.html", groups=[1,2])
