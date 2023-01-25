de57d1494d5999f997f28743d659b618dffecc65
Ivan <lapchenko.vanya@gmail.com>
2023-01-25 16:13:34 +0200

    Edit routes

--- crm_system/routes.py
+++ crm_system/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 from crm_system import app
 from crm_system.models.database import session
 from crm_system.models.group import Group
@@ -13,16 +13,19 @@ def index():
 @app.route("/")
 @app.route("/group_management", methods=["GET", "POST"])
 def group_management():
-    groups = session.query(Group)
+    all_data = session.query(Group).all()
+    all_data = [i.group_name for i in all_data]
     if request.method == "POST":
         group_name = request.form["gr_name"]
         student = Group(group_name=group_name)
         session.add(student)
         session.commit()
-    return render_template("group_management.html", group_names=groups)
+        session.close()
+        return redirect("group_management")
+    return render_template("group_management.html", group_names=all_data)
 
 
-@app.route("/group_list/<g_name>")
+@app.route("/student_management/<g_name>")
 def group_list(g_name):
     gr_id = session.query(Group).where(Group.group_name == g_name).first().id
     group = session.query(Student).where(Student.group == gr_id).all()
