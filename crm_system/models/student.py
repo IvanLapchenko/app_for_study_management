694b38753488bd776c67e9850db44072807cc26d
IvanLapchenko <lapchenko.vanya@gmail.com>
2023-02-03 16:14:22 +0200

    Move columns from user to student

--- crm_system/models/student.py
+++ crm_system/models/student.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from crm_system.models.database import Base
+from flask_login import UserMixin
 
 
-class Student(Base):
+class Student(UserMixin, Base):
     __tablename__ = 'student'
 
     id = Column(Integer, primary_key=True)
@@ -11,13 +12,18 @@ class Student(Base):
     age = Column(Integer)
     address = Column(String)
     group = Column(Integer, ForeignKey('groups.id'))
+    username = Column(String(100), unique=True)
+    password = Column(String(100))
 
-    def __init__(self, name: str, surname: str, age: int, address: str, id_group: int):
+    def __init__(self, name: str, surname: str, age: int, address: str, id_group: int, username: str, password: str):
         self.surname = surname
         self.name = name
         self.age = age
         self.address = address
         self.group = id_group
+        self.username = username
+        self.password = password
+
 
     # def __repr__(self):
     #     info = f"self.surname, self.name, self.age, self.address, self.group"
